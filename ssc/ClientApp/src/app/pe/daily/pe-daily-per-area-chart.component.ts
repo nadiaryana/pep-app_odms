9a5ecb66b100b89938dd1a53169de02067d74b2c
muhammadarifsp <85152766+muhammadarifsp@users.noreply.github.com>
2026-07-13 22:27:30 +0800

    fix: field chart high data

--- ssc/ClientApp/src/app/pe/daily/pe-daily-per-area-chart.component.ts
+++ ssc/ClientApp/src/app/pe/daily/pe-daily-per-area-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+﻿import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { HttpClient, HttpEventType, HttpParams, HttpResponse, HttpHeaders } from '@angular/common/http';
 import { formatDate } from '@angular/common';
 import { MatDatepicker } from '@angular/material';
@@ -320,286 +320,7 @@ export class PeDailyPerAreaChartComponent {
     // }
 
 	this.http.get('/api/pe/data', { params: params }).subscribe((res: any) => {
-	  console.log("resp",res.data);
-
-	  let tgl = res["data"].map(d => formatDate(d["date"], "dd-MMM-yy", "en-US"));
-	  let gross = res["data"].map(d => d["gross"]);
-	  let net = res["data"].map(d => d["net"]);
-	  let well = res["data"].map(d => d["well"]);
-	  let location = res["data"].map(d => d["location"]);
-	  let tgl_cek = 0;
-	  
-	  
-	  let g = res["data"].map(d => d["gross"]);
-	  let n = res["data"].map(d => d["net"]);
-	  let o = 0;
-	  let k = 0;
-	  let w = 0;
-	  let p = 0;
-	  let q = 0;
-	  let j = 0;
-	  
-	  
-	  let well_sgt = [];
-	  let g_sgt = [];
-	  let n_sgt = [];
-	  var tgl_sgt = [];
-	  
-	  let well_sbr = [];
-	  let g_sbr = [];
-	  let n_sbr = [];
-	  var tgl_sbr = [];
-	  
-	  let well_bd = [];
-	  let g_bd = [];
-	  let n_bd = [];
-	  var tgl_bd = [];	  
-	  
-	  let g1_sgt = [];
-	  let n1_sgt = [];
-	  let tg_sgt = [];
-	  
-	  let g1_sbr = [];
-	  let n1_sbr = [];
-	  let tg_sbr = [];
-	  
-	  let g1_bd = [];
-	  let n1_bd = [];
-	  let tg_bd = [];
-	  
-	  let well_cek = [];
-	  let g1 = [];
-	  let gg = [];
-	  let n1 = [];
-	  let nn = [];
-	  let welll = [];
-	  let tanggal = 0;
-	  let tanggal2 = 0;
-	  var tg = [];
-	  var tgg = [];
-	  
-	  let dt_well = well.length;
-	  let dt_date = [];
-	  let dt_grs_sgt = [];
-	  let dt_net_sgt = [];
-	  
-	  let dt_grs_sbr = [];
-	  let dt_net_sbr = [];
-	  
-	  let dt_grs_bd = [];
-	  let dt_net_bd = [];
-	  
-	  let dt_grss_sgt = [];	  
-	  let dt_nett_sgt = [];
-	  
-	  let dt_grss_sbr = [];	  
-	  let dt_nett_sbr = [];
-	  
-	  let dt_grss_bd = [];	  
-	  let dt_nett_bd = [];
-	  
-	  let dt_datee = [];
-	  
-	  
-	    
-	  this.per_area_chart_options["title"]["text"] = "Net Per Area";
-      this.per_area_chart_options["caption"]["text"] = formatDate(this.start_dateControl.value, 'd MMM y', 'en-US') + " - " + formatDate(this.end_dateControl.value, 'd MMM y', 'en-US');
-	  
-	  // console.log("Brp well: "+dt_well);
-	  // console.log("Tgl Brp: "+formatDate('2023-03-12T00:00:00+08:00', "dd-MMM-yy", "en-US"));
-	  // console.log("Tgl Brp: "+res["data"].map(d => d["date"]));
-	  
-	  
-	  for (var i = 0; i < tgl.length; i++){
-
-			// Cek Area SGT
-			const sgtAreaCodes = [
-				"GS-1", 
-				"GS-2", 
-				"GS-3", 
-				"GS-4", 
-				"GS-5", 
-				"GS-6", 
-				"TOS",
-			];
-			if (sgtAreaCodes.includes(location[i])) {
-				well_sgt[o] = well[i];
-				g_sgt[o] = gross[i];
-				n_sgt[o] = net[i];
-				tgl_sgt[o] = tgl[i];
-				
-				o++;
-			}
-			// Cek Area SBR
-			const sbrAreaCodes = ["MINI-P", "SBR-P", "TJ-BT", "TOS-SBT"];
-			if (sbrAreaCodes.includes(location[i])) {
-				well_sbr[k] = well[i];
-				g_sbr[k] = gross[i];
-				n_sbr[k] = net[i];
-				tgl_sbr[k] = tgl[i];
-
-				k++;
-			}
-			// Cek Area BD
-			const bdAreaCodes = ["BD"];
-			if (bdAreaCodes.includes(location[i])) {
-				well_bd[q] = well[i];
-				g_bd[q] = gross[i];
-				n_bd[q] = net[i];
-				tgl_bd[q] = tgl[i];
-
-				q++;
-			}
-		}
-		
-		
-		// SGT
-		for(o = 0; o <= well_sgt.length; o++){
-			// console.log("Dt sgt: "+o+". "+tgl_sgt[o]+" - "+well_sgt[o]+" - "+g_sgt[o]+" - "+n_sgt[o]);
-			
-			if (tanggal == tgl_sgt[o]){
-				g1_sgt[o] = g1_sgt[o-1] + g_sgt[o];
-				n1_sgt[o] = n1_sgt[o-1] + n_sgt[o];
-				
-				// console.log("Masuk if kah ? "+tanggal+" - "+g1[y])
-				tg_sgt[o] = tgl_sgt[o];
-			}
-			else{
-				// console.log("nilai g1_sgt: "+g1_sgt[o-1]);		
-				dt_grs_sgt[w] = [tg_sgt[o-1], g1_sgt[o-1]];
-				dt_net_sgt[w] = [tg_sgt[o-1], n1_sgt[o-1]];
-				dt_date[w] = tg_sgt[o-1];
-				
-				w++;
-				
-				tanggal = tgl_sgt[o];
-				g1_sgt[o] = g_sgt[o];
-				n1_sgt[o] = n_sgt[o];
-				
-				// console.log("nilai tg_sgt: "+tg_sgt[o-1]);
-			}
-		}
-		// console.log("nilai well_sgt: "+well_sgt.length);
-		// console.log("nilai dt_grs: "+dt_grs.length);
-		// console.log("nilai tanggal: "+tanggal);
-		// console.log("nilai w: "+w);
-		// console.log("nilai dt_grs: "+dt_grs_sgt);
-		
-		
-		
-		// SBR
-		for(k = 0; k <= well_sbr.length; k++){
-			// console.log("Dt sgt: "+o+". "+tgl_sgt[o]+" - "+well_sgt[o]+" - "+g_sgt[o]+" - "+n_sgt[o]);
-			
-			if (tanggal == tgl_sbr[k]){
-				g1_sbr[k] = g1_sbr[k-1] + g_sbr[k];
-				n1_sbr[k] = n1_sbr[k-1] + n_sbr[k];
-								
-				tg_sbr[k] = tgl_sbr[k];
-				
-				// console.log("nilai tgl: "+tg_sbr[o])
-				// console.log("nilai grs: "+g1_sbr[o])
-			}
-			else{
-				// console.log("nilai g1_sbr: "+g1_sbr[k-1]);
-				dt_grs_sbr[p] = [tg_sbr[k-1], g1_sbr[k-1]];
-				dt_net_sbr[p] = [tg_sbr[k-1], n1_sbr[k-1]];
-				dt_date[p] = tg_sbr[k-1];
-				
-				p++;
-				
-				tanggal = tgl_sbr[k];
-				g1_sbr[k] = g_sbr[k];
-				n1_sbr[k] = n_sbr[k];
-				
-				// console.log("nilai tg_sgt: "+tg_sgt[o-1]);
-			}
-		}
-		
-		
-		
-		// BD
-		for(q = 0; q <= well_bd.length; q++){
-			// console.log("Dt sgt: "+o+". "+tgl_sgt[o]+" - "+well_sgt[o]+" - "+g_sgt[o]+" - "+n_sgt[o]);
-			
-			if (tanggal == tgl_bd[q]){
-				g1_bd[q] = g1_bd[q-1] + g_bd[q];
-				n1_bd[q] = n1_bd[q-1] + n_bd[q];
-								
-				tg_bd[q] = tgl_bd[q];
-				
-				// console.log("nilai tgl: "+tg_sbj[q])
-				// console.log("nilai grs: "+g1_sbj[q])
-			}
-			else{
-				// console.log("nilai g1_sbj: "+g1_sbj[q-1]);
-				dt_grs_bd[j] = [tg_bd[q-1], g1_bd[q-1]];
-				dt_net_bd[j] = [tg_bd[q-1], n1_bd[q-1]];
-				dt_date[j] = tg_bd[q-1];
-				
-				j++;
-				
-				tanggal = tgl_bd[q];
-				g1_bd[q] = g_bd[q];
-				n1_bd[q] = n_bd[q];
-				
-				// console.log("nilai tg_sgt: "+tg_sgt[o-1]);
-			}
-		}
-		
-		
-		var y = 0;
-		for (var x = 1; x <= dt_grs_sgt.length; x++){
-			if (dt_grs_sgt[x] != null){
-				dt_datee[y] = dt_date[x];
-				dt_grss_sgt[y] = dt_grs_sgt[x];
-				dt_nett_sgt[y] = dt_net_sgt[x];
-				
-				dt_grss_sbr[y] = dt_grs_sbr[x];
-				dt_nett_sbr[y] = dt_net_sbr[x];
-				
-				dt_grss_bd[y] = dt_grs_bd[x];
-				dt_nett_bd[y] = dt_net_bd[x];
-				// console.log("nilai grss2: "+dt_datee);
-				y++;
-			}
-			else{
-				// console.log("nilai grss3: "+dt_grss[x])
-			}
-		}
-		// console.log("nilai date out: "+dt_date);
-		// console.log("nilai grss out: "+dt_grs_sbr);
-		// console.log("nilai nett out: "+dt_net_sbj);
-
-		
-		// -- FIXX --
-		this.per_area_chart_options["xAxis"][0]["categories"] = dt_datee;
-		this.per_area_chart_options["series"][0]["data"] = dt_nett_sgt;
-		this.per_area_chart_options["series"][1]["data"] = dt_nett_sbr;
-		this.per_area_chart_options["series"][2]["data"] = dt_nett_bd;
-		// this.per_area_chart_options["series"][3]["data"] = dt_nett_sbr;
-		// else if(well[i].includes("TJU")){
-				// if (tanggal == tgl[i]){
-					// g_sgt[i] = g_sgt[i-1] + gross[i];
-					// n_sgt[i] = n_sgt[i-1] + net[i];
-					
-					// tg[i] = tgl[i];
-					// dt_grs[i] = [tg[i], g_sgt[i]];
-					// dt_net[i] = [tg[i], n_sgt[i]];
-					// console.log("nilai grss: "+i+". "+dt_grs[i]);
-				// }
-				// else{
-					// o = 0
-					// tanggal = tgl[i]
-					// g_sgt[i] = gross[i]
-					// n_sgt[i] = net[i]
-					
-					// console.log("Masuk else kah ?")
-				// }
-				
-			// }
-
-		Highcharts.chart(this.per_area_chart_el.nativeElement, this.per_area_chart_options);
+	  this.renderPerArea(res["data"] || []);
 
 
     }, error => {
@@ -607,6 +328,47 @@ export class PeDailyPerAreaChartComponent {
     }, () => {
 
     });
+  }
+
+  // Agregasi Net Per Area (group-by tanggal). Kebal terhadap ukuran & urutan data.
+  renderPerArea(rows: any[]) {
+    const sgtAreaCodes = ["GS-1", "GS-2", "GS-3", "GS-4", "GS-5", "GS-6", "TOS"];
+    const sbrAreaCodes = ["MINI-P", "SBR-P", "TJ-BT", "TOS-SBT"];
+    const bdAreaCodes  = ["BD"];
+    const areaOf = (loc) =>
+      sgtAreaCodes.includes(loc) ? 'SGT' :
+      sbrAreaCodes.includes(loc) ? 'SBR' :
+      bdAreaCodes.includes(loc)  ? 'BD'  : null;
+
+    const byDate = {};
+    for (const d of rows) {
+      const area = areaOf(d["location"]);
+      if (!area) continue;
+      const dt = new Date(d["date"]);
+      if (isNaN(dt.getTime())) continue;
+      const key = dt.getFullYear() + '-' +
+                  String(dt.getMonth() + 1).padStart(2, '0') + '-' +
+                  String(dt.getDate()).padStart(2, '0');
+      if (!byDate[key]) byDate[key] = { SGT: 0, SBR: 0, BD: 0 };
+      byDate[key][area] += Number(d["net"]) || 0;
+    }
+
+    const keys = Object.keys(byDate).sort();
+    const categories = keys.map(k => {
+      const parts = k.split('-').map(Number);
+      return formatDate(new Date(parts[0], parts[1] - 1, parts[2]), "dd-MMM-yy", "en-US");
+    });
+
+    this.per_area_chart_options["title"]["text"] = "Net Per Area";
+    this.per_area_chart_options["caption"]["text"] =
+      formatDate(this.start_dateControl.value, 'd MMM y', 'en-US') + " - " +
+      formatDate(this.end_dateControl.value, 'd MMM y', 'en-US');
+    this.per_area_chart_options["xAxis"][0]["categories"] = categories;
+    this.per_area_chart_options["series"][0]["data"] = keys.map(k => byDate[k].SGT);
+    this.per_area_chart_options["series"][1]["data"] = keys.map(k => byDate[k].SBR);
+    this.per_area_chart_options["series"][2]["data"] = keys.map(k => byDate[k].BD);
+
+    Highcharts.chart(this.per_area_chart_el.nativeElement, this.per_area_chart_options);
   }
 
   refresh_Daily_Off() {
